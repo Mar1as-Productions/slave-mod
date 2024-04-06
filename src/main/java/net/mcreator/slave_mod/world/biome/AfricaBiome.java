55df0511d7ec3c5aa3a6f4ba578b5960372c6e28
Krisp1k <vladadoskar@gmail.com>
2024-04-06 14:17:40 +0200

    updated africa params

--- src/main/java/net/mcreator/slave_mod/world/biome/AfricaBiome.java
+++ src/main/java/net/mcreator/slave_mod/world/biome/AfricaBiome.java
@@ -15,26 +15,41 @@ import net.minecraft.world.gen.placement.IPlacementConfig;
 import net.minecraft.world.gen.placement.FrequencyConfig;
 import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
 import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraft.world.gen.feature.structure.ShipwreckConfig;
 import net.minecraft.world.gen.feature.structure.OceanRuinStructure;
 import net.minecraft.world.gen.feature.structure.OceanRuinConfig;
 import net.minecraft.world.gen.feature.structure.MineshaftStructure;
 import net.minecraft.world.gen.feature.structure.MineshaftConfig;
 import net.minecraft.world.gen.feature.SphereReplaceConfig;
 import net.minecraft.world.gen.feature.SeaGrassConfig;
-import net.minecraft.world.gen.feature.MultipleRandomFeatureConfig;
 import net.minecraft.world.gen.feature.IFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.BaseTreeFeatureConfig;
+import net.minecraft.world.gen.feature.AbstractTreeFeature;
+import net.minecraft.world.gen.blockstateprovider.SimpleBlockStateProvider;
+import net.minecraft.world.gen.IWorldGenerationReader;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.biome.DefaultBiomeFeatures;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.IWorldWriter;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.Direction;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.EntityClassification;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
 
 import net.mcreator.slave_mod.SlaveModModElements;
 
+import java.util.Set;
+import java.util.Random;
+
 import com.google.common.collect.Lists;
-import com.google.common.collect.ImmutableList;
 
 @SlaveModModElements.ModElement.Tag
 public class AfricaBiome extends SlaveModModElements.ModElement {
@@ -51,13 +66,13 @@ public class AfricaBiome extends SlaveModModElements.ModElement {
 
 	@Override
 	public void init(FMLCommonSetupEvent event) {
-		BiomeDictionary.addTypes(biome, BiomeDictionary.Type.MUSHROOM);
+		BiomeDictionary.addTypes(biome, BiomeDictionary.Type.MUSHROOM, BiomeDictionary.Type.PLAINS);
 		BiomeManager.addSpawnBiome(biome);
 		BiomeManager.addBiome(BiomeManager.BiomeType.DESERT, new BiomeManager.BiomeEntry(biome, 100));
 	}
 	static class CustomBiome extends Biome {
 		public CustomBiome() {
-			super(new Biome.Builder().downfall(0f).depth(0.1f).scale(0.2f).temperature(2f).precipitation(Biome.RainType.NONE)
+			super(new Biome.Builder().downfall(0f).depth(0.1f).scale(0.3f).temperature(2f).precipitation(Biome.RainType.NONE)
 					.category(Biome.Category.MUSHROOM).waterColor(-13420521).waterFogColor(-13619176).parent("ocean")
 					.surfaceBuilder(SurfaceBuilder.DEFAULT, new SurfaceBuilderConfig(Blocks.GRASS_BLOCK.getDefaultState(),
 							Blocks.DIRT.getDefaultState(), Blocks.DIRT.getDefaultState())));
@@ -68,11 +83,14 @@ public class AfricaBiome extends SlaveModModElements.ModElement {
 			DefaultBiomeFeatures.addTaigaRocks(this);
 			DefaultBiomeFeatures.addSedimentDisks(this);
 			DefaultBiomeFeatures.addFreezeTopLayer(this);
+			DefaultBiomeFeatures.addFossils(this);
+			DefaultBiomeFeatures.addInfestedStone(this);
+			DefaultBiomeFeatures.addMonsterRooms(this);
 			this.addStructure(Feature.STRONGHOLD.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
 			this.addStructure(Feature.MINESHAFT.withConfiguration(new MineshaftConfig(0.004D, MineshaftStructure.Type.NORMAL)));
 			this.addStructure(Feature.PILLAGER_OUTPOST.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
 			this.addStructure(Feature.VILLAGE.withConfiguration(new VillageConfig("village/plains/town_centers", 6)));
-			this.addStructure(Feature.OCEAN_MONUMENT.withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG));
+			this.addStructure(Feature.SHIPWRECK.withConfiguration(new ShipwreckConfig(false)));
 			this.addStructure(Feature.OCEAN_RUIN.withConfiguration(new OceanRuinConfig(OceanRuinStructure.Type.WARM, 0.3F, 0.9F)));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.FLOWER.withConfiguration(DefaultBiomeFeatures.DEFAULT_FLOWER_CONFIG)
 					.withPlacement(Placement.COUNT_HEIGHTMAP_32.configure(new FrequencyConfig(2))));
@@ -81,15 +99,13 @@ public class AfricaBiome extends SlaveModModElements.ModElement {
 			this.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.SEAGRASS.withConfiguration(new SeaGrassConfig(23, 0.3D))
 					.withPlacement(Placement.TOP_SOLID_HEIGHTMAP.configure(IPlacementConfig.NO_PLACEMENT_CONFIG)));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION,
-					Feature.RANDOM_SELECTOR
-							.withConfiguration(new MultipleRandomFeatureConfig(
-									ImmutableList.of(Feature.FANCY_TREE.withConfiguration(DefaultBiomeFeatures.FANCY_TREE_CONFIG).withChance(0.1F)),
-									Feature.NORMAL_TREE.withConfiguration(DefaultBiomeFeatures.JUNGLE_TREE_CONFIG)))
+					new CustomTreeFeature()
+							.withConfiguration((new BaseTreeFeatureConfig.Builder(new SimpleBlockStateProvider(Blocks.ACACIA_LOG.getDefaultState()),
+									new SimpleBlockStateProvider(Blocks.BIRCH_LEAVES.getDefaultState()))).baseHeight(20)
+											.setSapling((net.minecraftforge.common.IPlantable) Blocks.JUNGLE_SAPLING).build())
 							.withPlacement(Placement.COUNT_EXTRA_HEIGHTMAP.configure(new AtSurfaceWithExtraConfig(1, 0.1F, 1))));
 			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.SUGAR_CANE_CONFIG)
 					.withPlacement(Placement.COUNT_HEIGHTMAP_DOUBLE.configure(new FrequencyConfig(10))));
-			addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Feature.RANDOM_PATCH.withConfiguration(DefaultBiomeFeatures.CACTUS_CONFIG)
-					.withPlacement(Placement.COUNT_HEIGHTMAP_DOUBLE.configure(new FrequencyConfig(3))));
 			addFeature(GenerationStage.Decoration.UNDERGROUND_ORES,
 					Feature.DISK
 							.withConfiguration(new SphereReplaceConfig(Blocks.GRAVEL.getDefaultState(), 6, 2,
@@ -122,6 +138,153 @@ public class AfricaBiome extends SlaveModModElements.ModElement {
 		@Override
 		public int getSkyColor() {
 			return -589884;
+		}
+	}
+
+	static class CustomTreeFeature extends AbstractTreeFeature<BaseTreeFeatureConfig> {
+		CustomTreeFeature() {
+			super(BaseTreeFeatureConfig::deserialize);
+		}
+
+		@Override
+		protected boolean place(IWorldGenerationReader worldgen, Random rand, BlockPos position, Set<BlockPos> changedBlocks,
+				Set<BlockPos> changedBlocks2, MutableBoundingBox bbox, BaseTreeFeatureConfig conf) {
+			if (!(worldgen instanceof IWorld))
+				return false;
+			IWorld world = (IWorld) worldgen;
+			int height = rand.nextInt(5) + 20;
+			boolean spawnTree = true;
+			if (position.getY() >= 1 && position.getY() + height + 1 <= world.getHeight()) {
+				for (int j = position.getY(); j <= position.getY() + 1 + height; j++) {
+					int k = 1;
+					if (j == position.getY())
+						k = 0;
+					if (j >= position.getY() + height - 1)
+						k = 2;
+					for (int px = position.getX() - k; px <= position.getX() + k && spawnTree; px++) {
+						for (int pz = position.getZ() - k; pz <= position.getZ() + k && spawnTree; pz++) {
+							if (j >= 0 && j < world.getHeight()) {
+								if (!this.isReplaceable(world, new BlockPos(px, j, pz))) {
+									spawnTree = false;
+								}
+							} else {
+								spawnTree = false;
+							}
+						}
+					}
+				}
+				if (!spawnTree) {
+					return false;
+				} else {
+					Block ground = world.getBlockState(position.add(0, -1, 0)).getBlock();
+					Block ground2 = world.getBlockState(position.add(0, -2, 0)).getBlock();
+					if (!((ground == Blocks.GRASS_BLOCK || ground == Blocks.DIRT) && (ground2 == Blocks.GRASS_BLOCK || ground2 == Blocks.DIRT)))
+						return false;
+					BlockState state = world.getBlockState(position.down());
+					if (position.getY() < world.getHeight() - height - 1) {
+						setTreeBlockState(changedBlocks, world, position.down(), Blocks.DIRT.getDefaultState(), bbox);
+						for (int genh = position.getY() - 3 + height; genh <= position.getY() + height; genh++) {
+							int i4 = genh - (position.getY() + height);
+							int j1 = (int) (1 - i4 * 0.5);
+							for (int k1 = position.getX() - j1; k1 <= position.getX() + j1; ++k1) {
+								for (int i2 = position.getZ() - j1; i2 <= position.getZ() + j1; ++i2) {
+									int j2 = i2 - position.getZ();
+									if (Math.abs(position.getX()) != j1 || Math.abs(j2) != j1 || rand.nextInt(2) != 0 && i4 != 0) {
+										BlockPos blockpos = new BlockPos(k1, genh, i2);
+										state = world.getBlockState(blockpos);
+										if (state.getBlock().isAir(state, world, blockpos) || state.getMaterial().blocksMovement()
+												|| state.isIn(BlockTags.LEAVES) || state.getBlock() == Blocks.VINE
+												|| state.getBlock() == Blocks.BIRCH_LEAVES) {
+											setTreeBlockState(changedBlocks, world, blockpos, Blocks.BIRCH_LEAVES.getDefaultState(), bbox);
+										}
+									}
+								}
+							}
+						}
+						for (int genh = 0; genh < height; genh++) {
+							BlockPos genhPos = position.up(genh);
+							state = world.getBlockState(genhPos);
+							setTreeBlockState(changedBlocks, world, genhPos, Blocks.ACACIA_LOG.getDefaultState(), bbox);
+							if (state.getBlock().isAir(state, world, genhPos) || state.getMaterial().blocksMovement() || state.isIn(BlockTags.LEAVES)
+									|| state.getBlock() == Blocks.VINE || state.getBlock() == Blocks.BIRCH_LEAVES) {
+								if (genh > 0) {
+									if (rand.nextInt(3) > 0 && world.isAirBlock(position.add(-1, genh, 0)))
+										setTreeBlockState(changedBlocks, world, position.add(-1, genh, 0), Blocks.VINE.getDefaultState(), bbox);
+									if (rand.nextInt(3) > 0 && world.isAirBlock(position.add(1, genh, 0)))
+										setTreeBlockState(changedBlocks, world, position.add(1, genh, 0), Blocks.VINE.getDefaultState(), bbox);
+									if (rand.nextInt(3) > 0 && world.isAirBlock(position.add(0, genh, -1)))
+										setTreeBlockState(changedBlocks, world, position.add(0, genh, -1), Blocks.VINE.getDefaultState(), bbox);
+									if (rand.nextInt(3) > 0 && world.isAirBlock(position.add(0, genh, 1)))
+										setTreeBlockState(changedBlocks, world, position.add(0, genh, 1), Blocks.VINE.getDefaultState(), bbox);
+								}
+							}
+						}
+						for (int genh = position.getY() - 3 + height; genh <= position.getY() + height; genh++) {
+							int k4 = (int) (1 - (genh - (position.getY() + height)) * 0.5);
+							for (int genx = position.getX() - k4; genx <= position.getX() + k4; genx++) {
+								for (int genz = position.getZ() - k4; genz <= position.getZ() + k4; genz++) {
+									BlockPos bpos = new BlockPos(genx, genh, genz);
+									state = world.getBlockState(bpos);
+									if (state.isIn(BlockTags.LEAVES) || state.getBlock() == Blocks.BIRCH_LEAVES) {
+										BlockPos blockpos1 = bpos.south();
+										BlockPos blockpos2 = bpos.west();
+										BlockPos blockpos3 = bpos.east();
+										BlockPos blockpos4 = bpos.north();
+										if (rand.nextInt(4) == 0 && world.isAirBlock(blockpos2))
+											this.addVines(world, blockpos2, changedBlocks, bbox);
+										if (rand.nextInt(4) == 0 && world.isAirBlock(blockpos3))
+											this.addVines(world, blockpos3, changedBlocks, bbox);
+										if (rand.nextInt(4) == 0 && world.isAirBlock(blockpos4))
+											this.addVines(world, blockpos4, changedBlocks, bbox);
+										if (rand.nextInt(4) == 0 && world.isAirBlock(blockpos1))
+											this.addVines(world, blockpos1, changedBlocks, bbox);
+									}
+								}
+							}
+						}
+						if (rand.nextInt(4) == 0 && height > 5) {
+							for (int hlevel = 0; hlevel < 2; hlevel++) {
+								for (Direction Direction : Direction.Plane.HORIZONTAL) {
+									if (rand.nextInt(4 - hlevel) == 0) {
+										Direction dir = Direction.getOpposite();
+										setTreeBlockState(changedBlocks, world, position.add(dir.getXOffset(), height - 5 + hlevel, dir.getZOffset()),
+												Blocks.COCOA.getDefaultState(), bbox);
+									}
+								}
+							}
+						}
+						return true;
+					} else {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+		}
+
+		private void addVines(IWorld world, BlockPos pos, Set<BlockPos> changedBlocks, MutableBoundingBox bbox) {
+			setTreeBlockState(changedBlocks, world, pos, Blocks.VINE.getDefaultState(), bbox);
+			int i = 5;
+			for (BlockPos blockpos = pos.down(); world.isAirBlock(blockpos) && i > 0; --i) {
+				setTreeBlockState(changedBlocks, world, blockpos, Blocks.VINE.getDefaultState(), bbox);
+				blockpos = blockpos.down();
+			}
+		}
+
+		private boolean canGrowInto(Block blockType) {
+			return blockType.getDefaultState().getMaterial() == Material.AIR || blockType == Blocks.ACACIA_LOG || blockType == Blocks.BIRCH_LEAVES
+					|| blockType == Blocks.GRASS_BLOCK || blockType == Blocks.DIRT;
+		}
+
+		private boolean isReplaceable(IWorld world, BlockPos pos) {
+			BlockState state = world.getBlockState(pos);
+			return state.getBlock().isAir(state, world, pos) || canGrowInto(state.getBlock()) || !state.getMaterial().blocksMovement();
+		}
+
+		private void setTreeBlockState(Set<BlockPos> changedBlocks, IWorldWriter world, BlockPos pos, BlockState state, MutableBoundingBox mbb) {
+			super.func_227217_a_(world, pos, state, mbb);
+			changedBlocks.add(pos.toImmutable());
 		}
 	}
 }
